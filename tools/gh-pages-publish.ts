725eba2c55c99169d91a3988f28ba784481f63a8
izhaoqing <zhaoq0104@163.com>
2021-09-16 18:23:59 +0800

    ci: release

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -15,17 +15,17 @@ if (typeof pkg.repository === "object") {
 
 let parsedUrl = url.parse(repoUrl)
 let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
+// let ghToken = process.env.GH_TOKEN
 
 echo("Deploying docs!!!")
 cd("docs")
 touch(".nojekyll")
 exec("git init")
 exec("git add .")
-exec('git config user.name "zhaoqing"')
-exec('git config user.email "zhao.qing@trs.com.cn"')
+exec('git config user.name "izhaoqing"')
+exec('git config user.email "zhaoq0104@163.com"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
-  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
+  `git push --force --quiet "${repository}" master:gh-pages`
 )
 echo("Docs deployed!!")
